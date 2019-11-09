8a7d960f89649ca0117e13f6ecb4629d2d1a161c
aalevelaa <topiaceo@gmail.com>
2019-11-09 13:35:25 +0100

    We ask for the names of the players, somehow.

--- app/src/main/java/com/example/ruletadelasuerte/NamesActivity.java
+++ app/src/main/java/com/example/ruletadelasuerte/NamesActivity.java
@@ -1,17 +1,25 @@
 package com.example.ruletadelasuerte;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NamesActivity extends AppCompatActivity {
 
-    ImageView playButton;
-    String clickedButton;
+    private ImageView playButton;
+    private String clickedButton;
+
+    private static final String PLAYER_2 = "2";
+    private static final String PLAYER_3 = "3";
+    private static final String PLAYER_4 = "4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,24 @@ public class NamesActivity extends AppCompatActivity {
         Intent intent = getIntent();
         clickedButton = intent.getStringExtra("clicked_button");
 
-        Log.i(clickedButton, "BOTON PULSADO" + clickedButton);
+        switch (clickedButton)
+        {
+            case PLAYER_2:
+                createTextFields(PLAYER_2);
+                break;
+
+            case PLAYER_3:
+                createTextFields(PLAYER_3);
+                break;
+
+            case PLAYER_4:
+                createTextFields(PLAYER_4);
+                break;
+
+
+            }
+
+
 
         playButton = findViewById(R.id.playButton);
 
@@ -34,5 +59,26 @@ public class NamesActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    public void createTextFields(String str)
+    {
+        int total = Integer.parseInt(str);
+        LinearLayout lrLayout = findViewById(R.id.linearLayout);
+        TextView tv = new TextView(this);
+
+        tv.setText("¿CÓMO OS LLAMÁIS?");
+        lrLayout.addView(tv);
+
+        for(int i = 1; i <= total; i++)
+        {
+            TextView tvPlayer = new TextView(this);
+            tvPlayer.setText("PLAYER " + i);
+
+            lrLayout.addView(tvPlayer);
+            lrLayout.addView(new EditText(this));
+
+        }
     }
 }
--- app/src/main/java/com/example/ruletadelasuerte/PlayersMenuActivity.java
+++ app/src/main/java/com/example/ruletadelasuerte/PlayersMenuActivity.java
@@ -16,6 +16,7 @@ public class PlayersMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players_menu);
 
+        /*
         ImageView[] buttons = {findViewById(R.id.player2Button), findViewById(R.id.player3Button), findViewById(R.id.player4Button)};
 
         for (ImageView i: buttons)
@@ -31,6 +32,38 @@ public class PlayersMenuActivity extends AppCompatActivity {
                     }
             });
         }
+        */
+
+        ImageView button2 = findViewById(R.id.player2Button);
+        ImageView button3 = findViewById(R.id.player3Button);
+        ImageView button4 = findViewById(R.id.player4Button);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityNamesIntent = new Intent(getApplicationContext(), NamesActivity.class);
+                activityNamesIntent.putExtra("clicked_button", "2");
+                startActivity(activityNamesIntent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityNamesIntent = new Intent(getApplicationContext(), NamesActivity.class);
+                activityNamesIntent.putExtra("clicked_button", "3");
+                startActivity(activityNamesIntent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityNamesIntent = new Intent(getApplicationContext(), NamesActivity.class);
+                activityNamesIntent.putExtra("clicked_button", "4");
+                startActivity(activityNamesIntent);
+            }
+        });
 
     }
 }
