29059ea5f59bfb531d64a6b876ef67977a958b3d
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2019-11-14 13:56:31 +0100

    You spin me right'round, baby Right'round

--- app/src/main/java/com/example/ruletadelasuerte/GameActivity.java
+++ app/src/main/java/com/example/ruletadelasuerte/GameActivity.java
@@ -1,15 +1,30 @@
 package com.example.ruletadelasuerte;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.dynamicanimation.animation.FlingAnimation;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.content.res.XmlResourceParser;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
     private ImageView ruleta;
+    private AnimationSet animSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +33,34 @@ public class GameActivity extends AppCompatActivity {
 
         ruleta = findViewById(R.id.ruletaIntro);
 
-        FlingAnimation fling = new FlingAnimation(ruleta, DynamicAnimation.SCROLL_Y);
+        animSet = new AnimationSet(true);
+        animSet.setInterpolator(new DecelerateInterpolator());
+        animSet.setFillAfter(true);
+        animSet.setFillEnabled(true);
 
-        fling.setStartVelocity(-3)
-                .setMinValue(0)
-                .setMaxValue(20)
-                .setFriction(1.1f)
-                .start();
+        float r = (float) new Random().nextInt(360);
 
+        RotateAnimation animRotate = new RotateAnimation(0.0f, 2160 + r,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+
+        animRotate.setDuration(3000);
+        animRotate.setFillAfter(true);
+
+        animSet.addAnimation(animRotate);
+
+
+
+        ruleta.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ruleta.startAnimation(animSet);
+            }
+        });
     }
+
+
 }
