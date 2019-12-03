e17f64ab8b1dc262b1d0a86b56f9c1c615be047f
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2019-12-03 19:39:07 +0100

    Players icons added.

--- app/src/main/java/com/example/ruletadelasuerte/GameActivity.java
+++ app/src/main/java/com/example/ruletadelasuerte/GameActivity.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationSet;
-import android.view.animation.DecelerateInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
     private ImageView ruleta;
-    private AnimationSet animSet;
     private int numPlayers = 0;
     private String[] namePlayers;
+    private float lastRDiff = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,31 +37,30 @@ public class GameActivity extends AppCompatActivity {
     {
         ruleta = findViewById(R.id.ruletaIntro);
 
-        animSet = new AnimationSet(true);
-        animSet.setInterpolator(new DecelerateInterpolator());
-        animSet.setFillAfter(true);
-        animSet.setFillEnabled(true);
-
-        float r = (float) new Random().nextInt(360);
-
-        RotateAnimation animRotate = new RotateAnimation(0.0f, 2160 + r,
-                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
-                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
-
-
-        animRotate.setDuration(3000);
-        animRotate.setFillAfter(true);
-
-        animSet.addAnimation(animRotate);
-
-
-
         ruleta.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                ruleta.startAnimation(animSet);
+                float r = (float) new Random().nextInt(360);
+
+                final RotateAnimation animRotate = new RotateAnimation(0.0f, -r+5-360,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+
+                animRotate.setDuration(3000);
+                animRotate.setFillAfter(true);
+
+                ruleta.startAnimation(animRotate);
+
+                //lastRDiff = 360f - r;
+
+                TextView tv = findViewById(R.id.textView2);
+
+                String[] casillas = getResources().getStringArray(R.array.casillas);
+
+                tv.setText(casillas[(int) (Math.ceil(r/15)-1)]);
             }
         });
     }
